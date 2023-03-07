2ccea866214c80abb56e784d6d09456947a6f677
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2023-03-07 10:17:53 +0200

    Removed query methods related to the order business domain, they will be moved and implemented in the order microservice.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

--- java-path-spring-cloud-catalog-service/src/main/java/gr/codelearn/spring/cloud/showcase/catalog/service/ProductService.java
+++ java-path-spring-cloud-catalog-service/src/main/java/gr/codelearn/spring/cloud/showcase/catalog/service/ProductService.java
@@ -2,12 +2,7 @@ package gr.codelearn.spring.cloud.showcase.catalog.service;
 
 import gr.codelearn.spring.cloud.showcase.catalog.domain.Product;
 import gr.codelearn.spring.cloud.showcase.core.service.BaseService;
-import gr.codelearn.spring.cloud.showcase.core.transfer.KeyValue;
-
-import java.util.List;
 
 public interface ProductService extends BaseService<Product, Long> {
 	Product findBySerial(String serial);
-
-	List<KeyValue<String, Integer>> findProductSaleFrequency();
 }
--- java-path-spring-cloud-catalog-service/src/main/java/gr/codelearn/spring/cloud/showcase/catalog/service/ProductServiceImpl.java
+++ java-path-spring-cloud-catalog-service/src/main/java/gr/codelearn/spring/cloud/showcase/catalog/service/ProductServiceImpl.java
@@ -3,12 +3,9 @@ package gr.codelearn.spring.cloud.showcase.catalog.service;
 import gr.codelearn.spring.cloud.showcase.catalog.domain.Product;
 import gr.codelearn.spring.cloud.showcase.catalog.repository.ProductRepository;
 import gr.codelearn.spring.cloud.showcase.core.service.BaseServiceImpl;
-import gr.codelearn.spring.cloud.showcase.core.transfer.KeyValue;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -23,10 +20,5 @@ public class ProductServiceImpl extends BaseServiceImpl<Product> implements Prod
 	@Override
 	public Product findBySerial(String serial) {
 		return productRepository.findBySerial(serial);
-	}
-
-	@Override
-	public List<KeyValue<String, Integer>> findProductSaleFrequency() {
-		return productRepository.findProductSaleFrequency();
 	}
 }
